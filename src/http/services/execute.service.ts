c5c1e7be3b77de395371414a697ae71047e845e5
rgerrans <rgerrans@gmail.com>
2019-11-11 14:29:51 -0700

    Update execute.service.ts

--- src/http/services/execute.service.ts
+++ src/http/services/execute.service.ts
@@ -52,7 +52,7 @@ export class ExecuteService {
           case ExecuteCommandTypes.ThermostatTemperatureSetpoint:
           case ExecuteCommandTypes.ThermostatTemperatureSetRange:
           case ExecuteCommandTypes.ThermostatSetMode:
-          case ExecuteCommandTypes.FanSpeed:
+          case ExecuteCommandTypes.SetFanSpeed:
           case ExecuteCommandTypes.OpenClose:
             this.devices.updateDevicesState(deviceIds, execution.params, {
               requestId,
