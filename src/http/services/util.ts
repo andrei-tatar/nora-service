8d178d6076c84e57532e875f39708c6169f516cd
Andrei Tatar <tatarandrei@gmail.com>
2020-02-13 20:36:07 +0200

    use - separator for id

--- src/http/services/util.ts
+++ src/http/services/util.ts
@@ -1,8 +1,8 @@
 export function decompose(googleId: string) {
-    const parts = googleId.split('|');
+    const parts = googleId.split('-');
     return { id: parts[0], group: parts.slice(1).join("") };
 }
 
 export function compose({ id, group }: { id: string, group: string }) {
-    return `${id}|${group}`;
+    return `${id}-${group}`;
 }
